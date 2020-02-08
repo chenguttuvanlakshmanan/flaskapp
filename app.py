74596c9fd59264580575497b48f1bac3c88e3400
chenguttuvanlakshmanan <lchenmbss@gmail.com>
2020-02-09 00:42:25 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+# importing needed modules
+from flask import Flask, request
+from flask_restful import Resource, Api
+import pandas as pd
+import pickle
+
+#intializing flask app
+app = Flask(__name__)
+api = Api(app)
+
+def model_build(data):
+    model = pickle.load(open("model.pkl","rb"))
+    ypred = model.predict(data)
+    return str(ypred)
+
+#--------------------------------------------------------------------------------------------
+# Model got from pickle file
+#----------------------------------------------------------------------------------------------  
+class Topredict(Resource):
+    def post(self):
+        data = pd.DataFrame(request.get_json())
+        # build the model and insert in to db
+        status = model_build(data)
+        return { "result" : status}
+
+# api end point
+api.add_resource(Topredict,'/predict')
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ model.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_iris
+import pandas as pd
+
+iris = load_iris()
+x = pd.DataFrame(iris.data,columns=[iris.feature_names])
+y = iris.target
+
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+sv = SVC()
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.3)
+model = sv.fit(x,y)
+
+import pickle
+pickle.dump(model,open("model.pkl","wb"))
+
+
+
